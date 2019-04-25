49ce2faea838dbc723106b2779532c49f29b6d5f
silverlak3 <freefastfire@gmail.com>
2019-04-25 23:51:45 +0200

    Modifed deploy.sh and config.toml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#------------
+#Setup the public folder to work with the github io repo first! Why?
+#Github pages works off of the <USERNAME>.github.io repo. This is what gets rendered as a website. This script and all the source code content lives in a separate repo.
+#The command below creates a git submodule. Now when you run the hugo command to build your site to public, the created public directory will have a different remote origin (i.e. hosted GitHub.io repository).
+#git submodule add -b master git@github.com:<USERNAME>/<USERNAME>.github.io.git public
+#Then, all you have to do is run this script everytime you want to update the website. Once hugo finishes compiling, this script will upload only the compiled content over to the github.io page.
+#------------
+
+#If you want a custom commit message, add it as an argument to this script, deploy.sh "<commit message>"
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
